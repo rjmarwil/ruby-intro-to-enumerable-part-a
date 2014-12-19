6bdeccb67e54e6776ebc58cf7cf8f8a0a538ea63
EmilyPlatzer <emilyplatzer@gmail.com>
2014-12-19 15:08:34 -0700

    Added reduce method

---
+++ exercises/17_reduce_words.rb
@@ -0,0 +1,12 @@
+
+# ------ code above this line ------
+
+require 'rspec/autorun'
+
+RSpec.describe '#reduce' do
+  it 'returns an string of the concatonation plus space of words in the array' do
+    input = ['Super', 'cali', 'fragi', 'listic', 'expial', 'i', 'do', 'cious']
+
+    expect(reduce(input)).to eq('Supercalifragilisticexpialidocious')
+  end
+end
